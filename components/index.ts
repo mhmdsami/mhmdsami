a5210f7bc046397bfa9c8c86089c6536a30d85a5
Mohamed Sami <srmf2025@gmail.com>
2022-05-17 21:19:42 +0530

    reexport components

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { default as Layout } from "./Layout";
+export { default as Navbar } from "./Navbar";
+export { default as Landing } from "./Landing";
+export { default as Hero } from "./Hero";
+export { default as Footer } from "./Footer";
