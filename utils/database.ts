2ab568c8c5846b588643ba4c9c5474826f4e43cb
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-04 23:16:44 +0530

    add database client

---
+++ utils/database.ts
@@ -0,0 +1,17 @@
+import { Db, MongoClient } from 'mongodb';
+
+let db: Db;
+
+async function initializeClient(): Promise<Db> {
+    const client = await MongoClient.connect(process.env.DB_URI || "");
+    return client.db("portfolio");
+}
+
+const database = async (): Promise<Db> => {
+    if (!db) {
+        db = await initializeClient();
+    }
+    return db;
+};
+
+export default database;
