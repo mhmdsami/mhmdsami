b0bee74ff548eea4938372d0044a9be289bc2fd6
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-11 22:10:27 +0530

    apply theme

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,10 +4,10 @@ import {Layout} from "../components";
 const blog: NextPage = () => {
     return (
         <Layout pageName="Blog">
-            <div className="flex flex-col gap-4 mt-48 items-center">
+            <div className="flex flex-col gap-4 items-center">
                 <div className="text-4xl font-bold">Coming Soon 👀</div>
                 <div>
-                    For updates <a href="https://github.com/sm-sami/blog" className="bg-gray-100 px-1 py-0.5 rounded-md cursor-pointer">⭐ Star</a> the repo on GitHub
+                    For updates <a href="https://github.com/sm-sami/blog" className="bg-white text-black px-1 py-0.5 rounded-md cursor-pointer">⭐ Star</a> the repo on GitHub
                 </div>
                 <div>
                     View the latest deployment <a href="https://blog-sm-sami.vercel.app/" className="cursor-pointer font-bold hover:underline">here</a>
