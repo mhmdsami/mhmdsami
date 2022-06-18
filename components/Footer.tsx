727dc16edab06eb8a2541a2bd9f64353048c49b7
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-18 18:10:31 +0530

    modify public directory structure, change Project card theme

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
             </div>
             <ul className="flex gap-4">
                 {footerElements.map(({ site, href }, index) => (
-                    <li key={index} className="transition-all duration-300 hover:-translate-y-1"><a href={href}><Image src={`/socials/light/${site}.svg`} alt={site} width={32} height={32}/></a></li>
+                    <li key={index} className="transition-all duration-300 hover:-translate-y-1"><a href={href}><Image src={`/icons/light/${site}.svg`} alt={site} width={32} height={32}/></a></li>
                 ))}
             </ul>
             <div>crafted with <a href="https://nextjs.org/" className="font-bold hover:underline">Next.js</a> and <a href="https://tailwindcss.com/" className="font-bold hover:underline">tailwindcss</a></div>
--- components/Project.tsx
+++ components/Project.tsx
@@ -13,7 +13,7 @@ interface ProjectComponentProps {
 
 const Project = ({ name, children, image, tags, repo, deps }: ProjectComponentProps) => {
     return (
-        <div className="text-black bg-white rounded-md md:rounded-xl p-5 mt-4 mb-7">
+        <div className="bg-black rounded-md md:rounded-xl p-5 mt-4 mb-7">
             <div className="text-lg font-bold">{name}</div>
             {children &&
                 <div className="flex flex-col gap-3">
@@ -31,18 +31,18 @@ const Project = ({ name, children, image, tags, repo, deps }: ProjectComponentPr
                 <div className="flex flex-wrap gap-2 mt-5 w-2/3 md:w-full">
                     <div className="font-medium">built with </div>
                     {tags.map((tag, ind) => (
-                        <div key={ind} className="bg-black text-white px-2 rounded-lg font-medium h-min w-min"><Link href={`/skill/${tag}`}>{tag}</Link></div>
+                        <div key={ind} className="bg-red text-black-dark px-2 rounded-lg font-medium h-min w-min"><Link href={`/skill/${tag}`}>{tag}</Link></div>
                     ))}
                 </div>
                 <div className="flex gap-2 mt-5">
                     {repo &&
                         <a href={repo} className="transition-all duration-300 hover:-translate-y-1">
-                            <Image src={`/socials/dark/github.svg`}  alt="github logo" width={28} height={28}/>
+                            <Image src="/icons/light/github.svg"  alt="github logo" width={28} height={28}/>
                         </a>
                     }
                     {deps &&
                         <a href={deps} className="transition-all duration-300 hover:-translate-y-1">
-                            <Image src={`/website.svg`}  alt="website" width={28} height={28}/>
+                            <Image src="/icons/light/website.svg"  alt="website" width={28} height={28}/>
                         </a>
                     }
                 </div>
