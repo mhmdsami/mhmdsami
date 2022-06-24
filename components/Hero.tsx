79586c6bd1436467886a2793683a54c48ac0848c
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-24 20:15:53 +0530

    fix: font import

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = ( {name, greeting, tag} : HeroProps) => {
                 {tag}
             </div>
         </main>
-        <div className="flex place-content-center animate-bounce pt-20 text-red cursor-pointer select-none">
+        <div className="flex place-content-center animate-bounce pt-20 text-red cursor-pointer">
             <Link to="skills" smooth="true" duration={700}><svg stroke="currentColor" fill="currentColor" strokeWidth="1" viewBox="0 0 16 16" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"></path></svg>
             </Link>
         </div>
