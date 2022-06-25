3ca73a7c918ce7362a0d3ab0e5cb80cfdfcf2384
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-25 17:10:49 +0530

    refactor navbar and related components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,21 @@
-import Head from "next/head";
 import React from "react";
+import Head from "next/head";
 import { Navbar, Footer } from "./";
+import { NavbarRoute } from "./Navbar";
 
 interface LayoutProps {
     pageName?: string,
+    routes: Array<NavbarRoute>
     children: React.ReactNode
 }
 
-const Layout = ({pageName, children}: LayoutProps) => {
+const Layout = ({ pageName, children, routes }: LayoutProps) => {
     return (
         <div className="grid min-h-screen">
             <Head>
-                <title>Mohamed Sami{pageName ? ` | ${pageName}` : ''}</title>
+                <title>Mohamed Sami{pageName && ` | ${pageName}`}</title>
             </Head>
-            <Navbar/>
+            <Navbar routes={routes}/>
             <main>{children}</main>
             <div className="w-full place-self-end">
                 <Footer />
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,17 +1,24 @@
-import {Layout, Project} from "../components";
-import {GetServerSideProps, GetServerSidePropsContext} from "next";
-import {ProjectProps} from "./api/projects";
+import { Layout, Project } from "../components";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { ProjectProps } from "./api/projects";
 import React from "react";
+import { NavbarRoute } from "../components/Navbar";
 
 interface ProjectsPageProps {
     projects: Array<ProjectProps>;
 }
 
-const projects = ( {projects}: ProjectsPageProps ) => {
+const projects = ({ projects }: ProjectsPageProps) => {
+    const routes: Array<NavbarRoute> = [
+        { page: "home", href: "/"},
+        { page: "blog", href: "/blog"},
+        { page: "github", href: "https://github.com/sm-sami" }
+    ]
+
     return (
-        <Layout pageName="Projects">
+        <Layout pageName="Projects" routes={routes}>
             <div className="grid gap-x-7 content-margin">
-                {projects.map(( {name, image, repo, deps, tags, desc}: ProjectProps, index: number) => (
+                {projects.map(( { name, image, repo, deps, tags, desc }: ProjectProps, index: number) => (
                     <Project key={index} name={name} image={image} repo={repo} deps={deps} tags={tags}>
                         {desc}
                     </Project>
