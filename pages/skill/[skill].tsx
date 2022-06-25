fa1f02540cca5140b6e10de16eb133605e1fa946
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-25 17:17:43 +0530

    add nav routes

--- pages/skill/[skill].tsx
+++ pages/skill/[skill].tsx
@@ -2,14 +2,21 @@ import {Layout, Project} from "../../components";
 import type {GetServerSideProps, GetServerSidePropsContext} from "next";
 import {ProjectProps} from "../api/projects";
 import React from "react";
+import {NavbarRoute} from "../../components/Navbar";
 
 interface SkillPageProps {
     projects: Array<ProjectProps>;
 }
 
 const skill = ( { projects } : SkillPageProps) => {
+    const routes: Array<NavbarRoute> = [
+        { page: "projects", href: "/projects"},
+        { page: "blog", href: "/blog" },
+        { page: "github", href: "https://github.com/sm-sami" }
+    ]
+
     return (
-        <Layout>
+        <Layout routes={routes}>
             <div className="grid gap-x-7 content-margin">
                 {projects.map(( {name, image, repo, deps, tags, desc}: ProjectProps, index: number) => (
                     <Project key={index} name={name} image={image} repo={repo} deps={deps} tags={tags}>
