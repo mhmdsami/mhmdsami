62f2c4a27ded747e0091f0759650c3e29f28e11a
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-06-28 19:45:48 +0530

    :art: move routes for Navbar to constants

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,8 +1,8 @@
 import { Layout, Hero, Skills, FeaturedProjects } from "./";
 import { Slide } from 'react-awesome-reveal';
+import { homePageRoutes } from "../constants";
 import type { SkillSet } from "../pages/api/data";
-import { ProjectProps } from "../pages/api/projects";
-import type { NavbarRoute } from "./Navbar";
+import type { ProjectProps } from "../pages/api/projects";
 
 interface LandingProps {
     name: string;
@@ -13,15 +13,8 @@ interface LandingProps {
 }
 
 const Landing = ({ name, greeting, tag, skillSets, featuredProjects }: LandingProps) => {
-    const routes: Array<NavbarRoute> = [
-        { page: "skills", href: "skills", isSamePage: true },
-        { page: "projects", href: "/projects"},
-        { page: "blog", href: "/blog" },
-        { page: "github", href: "https://github.com/sm-sami" }
-    ]
-
     return (
-        <Layout pageName="Home" routes={routes}>
+        <Layout pageName="Home" routes={homePageRoutes}>
             <Slide triggerOnce={true} direction="down">
                 <Hero name={name} greeting={greeting} tag={tag}/>
             </Slide>
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,10 @@
-import type { NextPage } from "next";
 import { Layout } from "../components";
-import { NavbarRoute } from "../components/Navbar";
+import { blogPageRoutes } from "../constants";
+import type { NextPage } from "next";
 
 const blog: NextPage = () => {
-    const routes: Array<NavbarRoute> = [
-        { page: "home", href: "/"},
-        { page: "projects", href: "/projects"},
-        { page: "github", href: "https://github.com/sm-sami" }
-    ]
-
     return (
-        <Layout pageName="Blog" routes={routes}>
+        <Layout pageName="Blog" routes={blogPageRoutes}>
             <div className="flex flex-col gap-4 items-center">
                 <div className="text-4xl font-bold">Coming Soon 👀</div>
                 <div>
