b61a10c550cac6f6ef8a0a760e8ea9c543ba259e
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-07-04 20:00:05 +0530

    :bento: add resume

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
 interface ButtonProps {
     buttonContent: string;
     href: string;
+    download?: string;
 }
 
-const Button = ({ buttonContent, href }: ButtonProps) => {
+const Button = ({ buttonContent, href, download }: ButtonProps) => {
     return(
-        <a className="px-4 py-1 rounded-xl bg-red text-black font-bold transition-all duration-300 hover:scale-110" href={href}>
+        <a className="px-4 py-1 rounded-xl bg-red text-black font-bold transition-all duration-300 hover:scale-110" href={href} download={download}>
             {buttonContent}
         </a>
     )
