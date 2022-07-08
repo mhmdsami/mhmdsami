c686fb14f87c44012b6a8c6cb96d2e62a8fa167f
Mohamed Sami <64266012+sm-sami@users.noreply.github.com>
2022-07-08 18:23:16 +0530

    :lipstick: ui changes and change hover animation

--- components/Inspirations.tsx
+++ components/Inspirations.tsx
@@ -11,8 +11,8 @@ const Inspirations = ({ inspirations }: InspirationsComponentProps) => {
             <h1 className="text-base md:text-xl font-bold uppercase text-red">Inspired By</h1>
             <div className="flex flex-col gap-5">
                 {inspirations.map(({ name, github, portfolio }: InspirationProps, index: number) => (
-                    <div key={index} className="flex flex-row gap-3 md:gap-10 justify-between">
-                        <a className="text-xl md:text-2xl text-white font-bold transition-all duration-300 hover:scale-110 hover:text-red" href={portfolio}>{name}</a>
+                    <div key={index} className="flex flex-row gap-5 md:gap-0 md:w-[50vw] lg:w-[40vw] justify-between">
+                        <a className="text-xl md:text-2xl text-white font-bold transition-all duration-300 hover:translate-x-2 hover:text-red" href={portfolio}>{name}</a>
                         <div className="flex gap-3">
                             <a href={github} target="_blank" rel="noreferrer" className="transition-all duration-300 hover:-translate-y-1">
                                 <Image src="/icons/light/github.svg"  alt="github" width={28} height={28} />
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -8,11 +8,9 @@ const TechStack = ({ techStack }: TechStackComponentProps) => {
     return (
         <>
             <h1 className="text-base md:text-xl font-bold uppercase text-red">Built With</h1>
-            <div className="flex flex-col gap-3">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-y-3">
                 {techStack.map(({ name, url }: TechStackProps, index: number) => (
-                    <div key={index} className="text-xl text-white font-bold transition-all duration-300 hover:translate-x-2 hover:text-red">
-                        <a href={url}>{name}</a>
-                    </div>
+                    <a key={index} className="text-xl text-white font-bold transition-all duration-300 hover:translate-x-2 hover:text-red" href={url}>{name}</a>
                 ))}
             </div>
         </>
