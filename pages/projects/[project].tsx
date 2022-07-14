16411afd495994394bff9b566a748dc407be050f
Mohamed Sami <srmf2025@gmail.com>
2022-07-14 17:37:53 +0530

    :recycle: better error handling for non-existent projects

--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -1,34 +1,40 @@
-import {Layout, Tag} from "../../components";
+import { Error, Layout, Tag } from "../../components";
 import Image from "next/image";
+import Link from "next/link";
 import type { ProjectProps } from "../api/projects";
 import type { GetServerSideProps, GetServerSidePropsContext } from "next";
 
-const project = ({ name, tags, image }: ProjectProps) => {
+const project = ({ name, project, desc, tags, image }: ProjectProps) => {
     return(
-        <Layout pageName={name}>
-            <div className="content-padding">
-                {image && <Image src={`/projects/${image}`} alt={name} width={2880} height={1410} />}
-                <div className="flex gap-2">{tags.map((tag, index) => <Tag key={index}>{tag}</Tag>)}</div>
-            </div>
-        </Layout>
+        <div>
+            {name ?(
+                <Layout pageName={name}>
+                    <div className="content-margin flex flex-col gap-3">
+                        <h1 className="text-3xl font-bold">{name}</h1>
+                        <p>{desc}</p>
+                        <div className="flex flex-col md:flex-row md:w-3/4 gap-2">
+                            <p className="uppercase font-bold whitespace-nowrap">built with</p>
+                            <div className="flex flex-wrap md:flex-nowrap gap-2">{tags.map((tag, index) => <Tag key={index}><Link href={`/skills/${tag}`}>{tag}</Link></Tag>)}</div>
+                        </div>
+                        <div className="p-1" />
+                        <div className="self-center">{image && <Image src={`/projects/${image}`} alt={name} width={2880} height={1410} className="rounded-sm md:rounded-md" />}</div>
+                    </div>
+                </Layout>
+            ) : (
+                <Error pageName={project} errorCode="404" error="Project Not Found" accessedUrl={project} redirectTo="/projects" buttonContent="explore other projects" />
+            )}
+        </div>
     )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
     const { project } = context.query;
     const res = await fetch(`${process.env.API_BASE_URL}/api/projects/${project}`);
-    try {
-        const { name, tags, image } = await res.json();
+    const projectDetails = await res.json();
 
-        return {
-            props: { name, tags, image }
-        };
-    } catch (err) {
-        context.res.writeHead(302, { Location: '/404' });
-        context.res.end();
-
-        return { props: {} };
-    }
+    return {
+        props: { ...projectDetails, project }
+    };
 }
 
 export default project;
