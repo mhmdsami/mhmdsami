343fd45623f2bb223cb9a60561e54bff63624848
Sami <78439283+mhmdsami@users.noreply.github.com>
2022-10-02 10:07:07 +0530

    :sparkles: add About, update instagram (#20)
    Co-authored-by: Mohamed Sami <srmf2025@gmail.com>

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,18 +9,24 @@ interface HeroProps {
 const Hero = ({ name, greeting, tag }: HeroProps) => {
   return (
     <div className="content-padding flex min-h-[85vh] flex-col bg-gradient-to-b from-black-dark to-black">
-      <main className="my-28 flex flex-col justify-between md:flex-row">
-        <div className="text-4xl lg:text-5xl">
-          {greeting}!
-          <span className="inline-block origin-bottom-right animate-wave select-none">
+      <main className="my-4 sm:my-28 flex flex-col justify-between md:flex-row">
+        <div>
+          <div className="text-4xl lg:text-5xl">
+            {greeting}!
+            <span className="inline-block origin-bottom-right animate-wave select-none">
             👋
           </span>
-          <br />
-          I&apos;m <span className="font-emphasis text-red">{name}</span>
+            <br />
+            I&apos;m <span className="font-emphasis text-red">{name}</span>
+          </div>
+          <div className="flex select-none items-end text-md text-gray xl:text-xl">
+            {tag}
+          </div>
         </div>
-        <div className="flex select-none items-end text-xl text-gray xl:text-2xl">
-          {tag}
-        </div>
+        <p className="mt-5 md:m-0 text-lg lg:text-xl md:w-1/2">
+          A sophomore CSE undergrad who loves to <span className="font-bold">craft web experiences</span>.
+          Always ready to take up challenges to <span className="font-bold">make the internet a little better than before.</span>
+        </p>
       </main>
       <div className="flex animate-bounce cursor-pointer select-none place-content-center pt-20 text-red">
         <Link to="skills" smooth="true" duration={700}>
