523a7474c94d3ed61fa9132ee95aed5810abe220
Mohamed Sami <srmf2025@gmail.com>
2024-06-07 17:02:56 +0530

    chore: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Mohamed Sami",
+  description:
+    "I craft memorable web experiences, automate repetitive and boring tasks, fix bugs, and build cool stuff for the web.",
+};
 
 export default function RootLayout({
   children,
